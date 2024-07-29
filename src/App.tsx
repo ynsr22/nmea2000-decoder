811a4d05ee54a6d98db2cd6e701ab5ce3c1e79f6
Yanis <yanis@srairi.fr>
2024-07-29 23:18:59 +0200

    index.html small change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect } from 'react';
 import { AlertCircle, Info } from 'lucide-react';
 import { Alert, AlertDescription, AlertTitle } from './components/ui/alert';
 import { Tooltip } from 'react-tooltip'

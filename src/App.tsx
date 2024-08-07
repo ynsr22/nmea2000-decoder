0a0248396d49aebe588d4ce62bc335aedd7cc979
Yanis <yanis@srairi.fr>
2024-08-07 08:18:57 +0100

    Better responsive for pgn list small screen

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,12 @@ import { Analytics } from '@vercel/analytics/react';
 
 const App = () => (
   <React.StrictMode>
-    <div className="min-h-screen bg-gradient-to-br from-blue-100 to-red-200 flex items-center justify-center p-4">
+    <div className="min-h-screen bg-gradient-to-br from-blue-400 to-red-400 flex items-center justify-center p-4">
       <div className="flex flex-col md:flex-row md:space-x-8 max-w-6xl">
-        <div className="flex-grow bg-red-200 md:w-2/3">
+        <div className="flex-grow md:w-2/3">
           <NMEA2000Decoder />
         </div>
-        <div className="md:w-1/3 mt-5 md:mt-0 mx-auto">
+        <div className="md:w-1/3 mt-5 md:mt-0 mx-auto w-full">
           <PGNListDropdown />
         </div>
       </div>
